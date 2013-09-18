222b32187169e0bb099d5a4cc49a2dca284c414f
wisn0101 <wisn0101>
2013-09-18 15:26:57 -0500

    Jana Wisniewski workday3

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -3,6 +3,17 @@
 Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
+        Dim num1, num2, largerNum As Double
 
+        num1 = CDbl(txtFirstNum.Text)
+        num2 = CDbl(txtSecondNum.Text)
+
+        If (num1 > num2) Then
+            txtResult.Text = "Larger number is" & num1
+        ElseIf (num2 > num1) Then
+            txtResult.Text = "Larger number is" & num2
+        Else
+            txtResult.Text = "The two are equal."
+        End If
     End Sub
 End Class
